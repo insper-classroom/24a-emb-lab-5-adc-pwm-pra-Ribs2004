868366d30891b93839108d86b0d5e74fefb860a5
Pedro <pedrocribeironeto@gmail.com>
2024-04-10 20:10:12 +0000

    Lab5

--- main/main.c
+++ main/main.c
@@ -20,14 +20,64 @@ typedef struct adc {
     int val;
 } adc_t;
 
+void write_package(adc_t data) {
+    int val = data.val;
+    int msb = val >> 8;
+    int lsb = val & 0xFF;
+
+    uart_putc_raw(uart0, data.axis);
+    uart_putc_raw(uart0, lsb);
+    uart_putc_raw(uart0, msb);
+    uart_putc_raw(uart0, -1);
+}
+
+int dead_zone(int val) {
+    if (val > -150 && val < 150) {
+        return 0;
+    }
+    return val;
+}
+
+void x_task(void *p) {
+    adc_t data;
+    adc_init();
+    adc_gpio_init(27);
+
+    while (1) {
+        data.axis = 0;
+        adc_select_input(1);
+        data.val = dead_zone((adc_read() - 2048) / 8) / 10;
+        xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }
+}
+
+void y_task(void *p) {
+    adc_t data;
+    adc_init();
+    adc_gpio_init(26);
+
+    while (1) {
+        data.axis = 1;
+        adc_select_input(0);
+        data.val = dead_zone((adc_read() - 2048) / 8) / 10;
+        xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }
+}
+
 
 void uart_task(void *p) {
     adc_t data;
 
     while (1) {
         xQueueReceive(xQueueAdc, &data, portMAX_DELAY);
+        // printf("\naxis: %d, %d\n", data.axis, data.val);
+        write_package(data);
+        // printf("Axis: %d, Value: %d\n", data.axis, data.val);
     }
 }
+
 
 int main() {
     stdio_init_all();
@@ -35,6 +85,8 @@ int main() {
     xQueueAdc = xQueueCreate(32, sizeof(adc_t));
 
     xTaskCreate(uart_task, "uart_task", 4096, NULL, 1, NULL);
+    xTaskCreate(x_task, "x_task", 4096, NULL, 1, NULL);
+    xTaskCreate(y_task, "y_task", 4096, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
